95e23858964e623bc8cb4d54a7117c1be5f8f7fc
z5045582 <z5045582@cse.unsw.EDU.AU>
2016-09-01 20:47:58 +1000

    web scraping isn't as cool as whales

---
+++ lab06/prereq.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -w
+%result = ();
+$url = "http://www.handbook.unsw.edu.au/postgraduate/courses/2015/$ARGV[0].html";
+open F, "wget -q -O- $url|" or die;
+while ($line = <F>) {
+    if ($line =~ 'Prerequisite') {
+	@x = split /<p>/, $line;
+	$x[1] =~ s/Prerequisite: //g;
+	$x[1] =~ s/<.+?>//g;
+	$x[1] =~ s/\.//g;
+	@get = split / /,$x[1];
+    }
+}
+foreach $r (@get) {
+    if ($r =~ '^\w{4}\d{4}$') {
+	$result{$r} = 1;
+    }
+}
+
+$url = "http://www.handbook.unsw.edu.au/undergraduate/courses/2015/$ARGV[0].html\
+";
+open F, "wget -q -O- $url|" or die;
+while ($line = <F>) {
+    if ($line =~ 'Prerequisite') {
+        @x = split /<p>/, $line;
+        $x[1] =~ s/Prerequisite: //g;
+        $x[1] =~ s/<.+?>//g;
+        $x[1] =~ s/\.//g;
+        @get = split / /,$x[1];
+    }
+}
+foreach $r (@get) {
+    if ($r =~ '^\w{4}\d{4}$') {
+	$result{$r} = 1;
+    }
+}
+
+foreach $r (sort keys %result) {
+    print "$r\n";
+}
