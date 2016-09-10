363276dfaac9ab21f01b829aec18126214bbc64a
z5045582 <z5045582@cse.unsw.EDU.AU>
2016-09-11 02:14:05 +1000

    frequency complete

---
+++ lab07/frequency.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+if ($#ARGV != 0){
+    print("Please give one word!\n");
+    exit(0);
+}
+$word = lc($ARGV[0]);
+foreach $file (glob "poems/*.txt") {
+    open(F, $file);
+    @f = <F>;
+    $wcount = 0;
+    $tcount = 0;
+    foreach $l (@f){
+	$l =~ s/'s/ s/g;
+	$l =~ s/[^a-zA-Z]/ /g;
+	@line = split/\s+/, lc($l);
+    foreach $w (@line){
+        if ($w ne "") {
+            $tcount++;
+        }
+        if ($w =~ /^$word$/){
+            $wcount++;
+        }
+    }
+    }
+    $file =~ s/poems\/|.txt//g;
+    $file =~ s/_/ /g;
+    printf("%4d/%6d = %.9f %s\n",$wcount,$tcount,$wcount/$tcount,$file);
+
+}
